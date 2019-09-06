499642d339e4f2dd18a274d1e7693100c716e34b
ACR0S <alexcastroros@hotmail.com>
2019-09-06 18:00:02 +0200

    add pandas to requirements, parse and write csv to db

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
+import csv
 import json
+import pandas as pd
 import sys
 
 from pymongo import MongoClient
@@ -8,10 +10,9 @@ NEIGHBOURHOODS_DATA = './data/neighbourhoods_madrid.json'
 HOSTS_DATA = './data/hosts_madrid.csv'
 
 
-def read_csv(csv_path):
-    with open(csv_path, 'r') as f:
-        csv_file = json.load(f)
-    return csv_file
+def csv_to_json(csv_path):
+    df = pd.read_csv(csv_path)
+    return df.to_dict(orient='records')
 
 
 def read_json(json_path):
@@ -24,7 +25,7 @@ def parse_json(original_json):
     return original_json["features"]
 
 
-def write_to_db(element, collection):
+def write_element_to_db(element, collection):
     collection.insert_one(element)
 
 
@@ -45,14 +46,22 @@ def main(neighbourhoods_path, hosts_path):
         sys.stderr.write("Could not connect to MongoDB: {}".format(e))
         sys.exit(1)
 
-    db = create_db(client, 'geo_db')
-    collection = create_collection(db, 'geo_collection')
+    db = create_db(client, 'geo_db')  # Create dataBase
+    neighbourhoods_collection = create_collection(db, 'neighbourhoods')  # Creates neighbourhoods collection
+    hosts_collection = create_collection(db, 'hosts')  # Create hosts collection
 
-    neighbourhoods_data = read_json(neighbourhoods_path)
+    neighbourhoods_data = read_json(neighbourhoods_path)  # Parse neighbourhoods data
     parsed_neighbourhoods_data = parse_json(neighbourhoods_data)
 
+    hosts_collection_data = csv_to_json(HOSTS_DATA)  # Parse hosts data
+
+
+    # Write to dataBase
     for element in parsed_neighbourhoods_data:
-        write_to_db(element, collection)
+        write_element_to_db(element, neighbourhoods_collection)
+
+    for element in hosts_collection_data:
+        write_element_to_db(element, hosts_collection)
 
 
 if __name__ == '__main__':
